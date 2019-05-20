326a516338aaa6bcbfd54bf48e074575874074c5
Ricidleiv Tondatto <tondatto@gmail.com>
2019-05-20 11:42:11 -0400

    Break long line Import-Csv

--- extract_student_inep_from_file.ps1
+++ extract_student_inep_from_file.ps1
@@ -6,4 +6,6 @@ $outputfile = "Alunos_MatriculaInicial_INEP.txt"
 Select-String $fileinep -Pattern $regex_pattern_student_line | foreach {$_.Line} | Out-File -FilePath $outputfile
 
 $csvoutput = "Aluno_Sigeduca_INEP.txt"
-Import-Csv $outputfile -Delimiter "|" -Header c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,c31,c32,c33,c34,c35,c36,c37,c38,c39 | Select c3,c4 | Export-Csv $csvoutput -Delimiter ";"
+Import-Csv $outputfile -Delimiter "|" -Header c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,c31,c32,c33,c34,c35,c36,c37,c38,c39 .
+    | Select c3,c4 
+    | Export-Csv $csvoutput -Delimiter ";"
